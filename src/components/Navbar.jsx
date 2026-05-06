2d0d96c7520084d9b457da38b3c039c35df085b1
Stephen-m001 <ndrtstephenm@gmail.com>
2026-05-06 08:27:33 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,96 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate, Link } from 'react-router-dom'
 
 const Navbar = () => {
-  return (
-        <section class="row">
-            <div class="col-md-12">
-                {/* <!-- a nav with the navbar content  --> */}
-                <nav class="navbar navbar-expand-md  "style={{backgroundColor: "#00FF7F"}}>
-                    <a href="" class="navbar-brand text-dark" style={{}}>Zuri Gaming</a>
-                    <button class="navbar-toggler" data-bs-target="#navbarcollapse" data-bs-toggle="collapse">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    {/* <!-- a division containing the links  --> */}
-                    <div class="collapse navbar-collapse" id="navbarcollapse">
-                        <div class="navbar-nav">
-                            <a href="/" class="nav-link ">Home</a>
-                            <a href="/addproduct" class="nav-link ">Add Product</a>
-                            <a href="/signup" class="nav-link ">Sign Up</a>
-                            <a href="/signin" class="nav-link ">Sign In </a>
+  const [username, setUsername] = useState("")
+  const navigate = useNavigate()
 
-                        </div>
-                    </div>
-                </nav>
+  // load login state
+  useEffect(() => {
+    const user = localStorage.getItem("username")
+    if (user) {
+      setUsername(user)
+    }
+  }, [])
+
+  // logout function
+  const handleLogout = () => {
+    localStorage.removeItem("username")
+    localStorage.removeItem("user")
+    setUsername("")
+    navigate("/signin")
+  }
+
+  return (
+    <section className="row">
+      <div className="col-md-12">
+
+        <nav
+          className="navbar navbar-expand-md"
+          style={{ backgroundColor: "#00FF7F" }}
+        >
+          {/* Brand */}
+          <Link to="/" className="navbar-brand text-dark">
+            Zuri Gaming
+          </Link>
+
+          <button
+            className="navbar-toggler"
+            data-bs-target="#navbarcollapse"
+            data-bs-toggle="collapse"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarcollapse">
+
+            <div className="navbar-nav me-auto">
+
+              {/* Home */}
+              <Link to="/" className="nav-link">Home</Link>
+
+              {/* 🚫 only show if logged in */}
+              {username && (
+                <Link to="/addproduct" className="nav-link">
+                  Add Product
+                </Link>
+              )}
+
+              {/* If NOT logged in */}
+              {!username && (
+                <>
+                  <Link to="/signup" className="nav-link">Sign Up</Link>
+                  <Link to="/signin" className="nav-link">Sign In</Link>
+                </>
+              )}
+
             </div>
-        </section>
+
+            {/* 👤 LOGIN STATUS AREA */}
+            <div className="d-flex align-items-center gap-2">
+
+              {username ? (
+                <>
+                  <span className="text-dark fw-bold">
+                    Logged in as: {username}
+                  </span>
+
+                  <button
+                    onClick={handleLogout}
+                    className="btn btn-danger btn-sm"
+                  >
+                    Logout
+                  </button>
+                </>
+              ) : null}
+
+            </div>
+
+          </div>
+        </nav>
+
+      </div>
+    </section>
   )
 }
 
